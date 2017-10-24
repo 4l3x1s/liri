5f617e58ca63135d4a6b97afca69ef15599b52f6
Grace Park <gracepark@github.com>
2017-10-23 20:49:44 -0700

    adding first 3 commands

--- keys.js
+++ keys.js
@@ -1,10 +1,13 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'm0h5qdVM6w64n2P48O8UuD0jM',
-  consumer_secret: 'mrZJVgDNwanrekeY0JXK6mAB3KiRrj97tyHJCgr5llvX4R6W1d',
-  access_token_key: '2181065000-Xdpbz4xMZ5cAmN0T78TYVciG8k6d0JZcqnGUmIT',
-  access_token_secret: 'oV1L97xGKYy2bBNDZeWHX5v77a6W8h3lsrZ32Q5oIHpAs',
+exports.twitterKeys = {
+  consumer_key: 'ZLR9vOGXXwRBvNXDyKwMgxtAB',
+  consumer_secret: 'tlXOyeccqnAuPU5bRnB8eKaiBMPo95x6YcpKJHddfaZgdXMl13',
+  access_token_key: '922238213778845696-5zcD0KF2GBqaWDTz4sDLcVlQcKRKoLb',
+  access_token_secret: 'goPP3u8Wx8D1dZfC1bYZu2fOAU2jfrWCNiaGikOk5j1Rs',
 }
 
-module.exports = twitterKeys;
+exports.spotifyKeys = {
+	id: '5cd1d172f98d42cd9296df65e920a800',
+	secret: '13d2cf2f85d04119bdebc354afceef57',
+}
--- liri.js
+++ liri.js
@@ -1,31 +1,89 @@
-var keys = require("keys.js");
+var keys = require("./keys.js");
+
 var request = require('request');
-var twitter = require('twitter');
-var spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var fs = require('fs');
+var client = new Twitter(keys.twitterKeys);
+var input = process.argv;
+var action = input[2];
+var inputs = input[3];
 
 switch (action) {
-	case "twitter":
+	case "my-tweets":
 	twitter();
 	break;
 
-	case "spotify":
+	case "spotify-this-song":
 	spotify();
 	break;
 
-	case "movie":
+	case "movie-this":
 	movie();
 	break;
 
-	case "doit":
+	case "do-what-it-says":
 	doit();
 	break;
+};
+
+function twitter() {
+
+	var params = {screen_name: 'grace_testing01', count: 20};
+	
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				// console.log(tweets);
+				for (i = 0; i < tweets.length; i ++){
+					console.log("Tweet: " + "'" + tweets[i].text + "'" + " Created At: " + tweets[i].created_at);
+				}
+			} else {
+				console.log(error);
+			}
+		});
+
+}
+
+function spotify() {
+
+	var spotify = new Spotify(keys.spotifyKeys);
+
+	spotify.search({ type: 'track', query: inputs }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		var songInfo = data.tracks.items[0];
+		console.log("Artist(s): " + songInfo.artists[0].name)
+		console.log("Song Name: " + songInfo.name)
+		console.log("Preview Link: " + songInfo.preview_url)
+		console.log("Album: " + songInfo.album.name)
+	});
 }
 
 
-var client = new Twitter({
-  consumer_key: keys.twitter_keys.consumer_key,
-  consumer_secret: keys.twitter_keys.consumer_secret,
-  access_token_key: keys.twitter_keys.access_token_key,
-  access_token_secret: keys.twitter_keys.access_token_secret
-});
+function movie() {
+
+	var movieName = inputs;
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+
+		if (!error && response.statusCode === 200) {
+
+		    console.log("Title: " + JSON.parse(body).Title);
+		    console.log("Release Year: " + JSON.parse(body).Year);
+		    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+		    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+		    console.log("Country: " + JSON.parse(body).Country);
+		    console.log("Language: " + JSON.parse(body).Language);
+		    console.log("Plot: " + JSON.parse(body).Plot);
+		    console.log("Actors: " + JSON.parse(body).Actors);
+		}
+	});
+};
+
+// function doit() {
+
+// };
+
